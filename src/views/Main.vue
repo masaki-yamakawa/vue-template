ec998cf02ce21e1653a6484b641dfab88c72b797
masaki.yamakawa <masaki.yamakawa@ulsystems.co.jp>
2019-07-21 09:02:31 +0900

    Change view lane layout

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,24 +1,16 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <h3>First Lane</h3>
-      <draggable id="first" :list="view1" class="list-group" draggable=".item" group="group1">
-        <div class="list-group-item item" v-for="view in views1" :key="view.id">
-          {{ view.name }}
-          <br />
-          <iframe :src="view.url" class="iframe" />
-        </div>
-      </draggable>
-    </div>
-    <div class="col">
-      <h3>Second Lane</h3>
-      <draggable id="second" :list="view2" class="list-group" draggable=".item" group="group1">
-        <div class="list-group-item item" v-for="view in views2" :key="view.id">
-          {{ view.name }}
-          <br />
-          <iframe :src="view.url" class="iframe" />
-        </div>
-      </draggable>
+  <div class="view-lane-container">
+    <div v-for="(views, id) in viewsLanes" :key="id" class="views-lanes">
+      <div class="views-lane">
+        <draggable :options="{group:'group', animation: 150}">
+          <i class="fa fa-align-justify handle"></i>
+          <div v-for="view in views" :key="view.id" class="view-lane item">
+            {{ view.name }}
+            <br />
+            <iframe :src="view.url" class="iframe-view" />
+          </div>
+        </draggable>
+      </div>
     </div>
   </div>
 </template>
@@ -33,33 +25,26 @@ import Draggable from "vuedraggable";
   },
 })
 export default class Main extends Vue {
-  private views1 = [
-    { id: 0, name: "Embeded View 1-1", url: "http://maps.google.co.jp/maps?&output=embed" },
-    { id: 1, name: "Embeded View 1-2", url: "/embed" },
-    { id: 2, name: "Embeded View 1-3", url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.4425090625195!2d136.88223540000004!3d35.170521900000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600376e794d78b89%3A0x81f7204bf8261663!2z5ZCN5Y-k5bGL6aeF!5e0!3m2!1sja!2sjp!4v1433317763525" },
-  ];
-
-  private views2 = [
-    { id: 3, name: "Embeded View 2-1", url: "/embed" },
-    { id: 4, name: "Embeded View 2-2", url: "https://www.youtube.com/embed/sk6uU8gb8PA?rel=0" },
+  private viewsLanes = [
+    [
+      { id: 0, name: "Embeded View 1-1", url: "http://maps.google.co.jp/maps?&output=embed" },
+      { id: 1, name: "Embeded View 1-2", url: "/embed" },
+      // tslint:disable-next-line:max-line-length
+      { id: 2, name: "Embeded View 1-3", url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.4425090625195!2d136.88223540000004!3d35.170521900000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600376e794d78b89%3A0x81f7204bf8261663!2z5ZCN5Y-k5bGL6aeF!5e0!3m2!1sja!2sjp!4v1433317763525" },
+    ],
+    [
+      { id: 3, name: "Embeded View 2-1", url: "/embed" },
+      { id: 4, name: "Embeded View 2-2", url: "https://www.youtube.com/embed/sk6uU8gb8PA?rel=0" },
+    ],
   ];
 }
 </script>
 
 <style scoped>
-.list-group {
-  min-height: 20px;
+.view-lane-container {
+  margin: 65px auto;
 }
-.list-group-item {
-  cursor: move;
-}
-.list-group-item i {
-  cursor: pointer;
-}
-.row {
-  margin: 50px auto;
-}
-.col {
+.views-lanes {
   width: 50%;
   display: block;
   float: left;
@@ -67,7 +52,14 @@ export default class Main extends Vue {
   list-style-type: none;
   text-align: center;
 }
-.iframe {
+.views-lane {
+  min-height: 400px;
+}
+.view-lane {
+  cursor: move;
+  cursor: pointer;
+}
+.iframe-view {
   width: 80%;
   height: 400px;
 }
